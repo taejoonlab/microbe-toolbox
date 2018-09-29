fc7c99bf5ae404d71040cbf204515709657d42e3
Taejoon Kwon <tkwon@unist.ac.kr>
2018-09-29 17:43:57 +0900

    A script to make non-redundant DNA sequences (From NuevoTx)

--- kmer/make-NR_nseq.sh
+++ kmer/make-NR_nseq.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 DIR_RRNA_FA=$1
-KMER_LEN=20
-
 DIR_SCRIPT=$(dirname $0)
 
 for FA in $(ls $DIR_RRNA_FA/*fa)
 do
   echo $FA
-  $DIR_SCRIPT/make-rRNA-kmer_freq.py $FA $KMER_LEN
+  $DIR_SCRIPT/make-NR_nseq.py $FA
 done
