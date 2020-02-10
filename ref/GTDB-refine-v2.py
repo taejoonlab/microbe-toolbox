2cc8c256af95163cb41f792b6d1c1efb91da7c07
Taejoon Kwon <tkwon@unist.ac.kr>
2020-02-10 17:17:20 +0900

    Revert the overwritten file.

--- ref/GTDB-refine-v2.py
+++ ref/GTDB-refine-v2.py
@@ -1,56 +1,40 @@
 #!/usr/bin/env python3
 import sys
-import os
+import gzip
 
-rc = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G', 'N': 'N'}
-
-
-def revcomp(tmp_seq):
-    return ''.join([rc[x] for x in tmp_seq[::-1]])
-
-
-filename_vulgar = sys.argv[1]
-filename_fa = '%s.fa' % filename_vulgar.split('.')[1]
-filename_out = filename_fa.replace('_v2.fa', '') + '_v3.fa'
-filename_log = filename_fa.replace('_v2.fa', '') + '_v3.log'
-
-if not os.access(filename_fa, os.R_OK):
-    sys.stderr.write('%s is not available. Check it first\n' % filename_fa)
-    sys.exit(1)
-
-str_info = dict()
-f_vulgar = open(filename_vulgar, 'r')
-for line in f_vulgar:
-    tokens = line.strip().split()
-    str_info[tokens[5]] = tokens[8]
-f_vulgar.close()
+# Input: fna files from GTDB
+filename_fna = sys.argv[1]
 
 seq_list = dict()
-f_fa = open(filename_fa, 'r')
-for line in f_fa:
+f_fna = open(filename_fna, 'r')
+if filename_fna.endswith('.gz'):
+    f_fna = gzip.open(filename_fna, 'rt')
+
+for line in f_fna:
     if line.startswith('>'):
         tmp_h = line.strip().lstrip('>')
+        tmp_id = tmp_h.split()[0]
+        tmp_sp_name = tmp_h.split(';')[-1].split('[')[0].strip()
+        tmp_sp_name = tmp_sp_name.replace('s__', '')
+        tmp_sp_name = tmp_sp_name.replace(' ', '_')
+
+        tmp_h = "%s|%s" % (tmp_sp_name, tmp_id)
         seq_list[tmp_h] = []
     else:
         seq_list[tmp_h].append(line.strip())
-f_fa.close()
+f_fna.close()
 
-f_out = open(filename_out, 'w')
-f_log = open(filename_log, 'w')
+# Set the sequence length criteria based on the mode.
+mode_len = 1537
+min_len = mode_len - 40
+max_len = mode_len + 40
+
 for tmp_h in sorted(seq_list.keys()):
     tmp_seq = ''.join(seq_list[tmp_h])
-    if tmp_h not in str_info:
-        sys.stderr.write('Not available: %s\n' % tmp_h)
-        f_log.write('Not available: %s\n' % tmp_h)
+
+    if len(tmp_seq) < min_len:
+        continue
+    if len(tmp_seq) > max_len:
         continue
 
-    tmp_residual = set(tmp_seq) - set(['A', 'T', 'G', 'C', 'N'])
-    for tmp_r in list(tmp_residual):
-        tmp_seq = tmp_seq.replace(tmp_r, 'N')
-
-    if str_info[tmp_h] == '+':
-        f_out.write(">%s\n%s\n" % (tmp_h, tmp_seq))
-    elif str_info[tmp_h] == '-':
-        f_out.write(">%s\n%s\n" % (tmp_h, revcomp(tmp_seq)))
-f_out.close()
-f_log.close()
+    print(">%s\n%s" % (tmp_h, tmp_seq))
